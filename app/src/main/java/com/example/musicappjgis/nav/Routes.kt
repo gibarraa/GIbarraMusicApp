360d2832f1144982c2f99f1b8b0d381f5f5bca2c
g.ibarraa <g.ibarraas@gmail.com>
2025-10-17 17:39:03 -0600

    crea estructura de paquetes y HomeScreen placeholder, se crearon archivos y carpetas para dar estructura y los cuales modificaré más adelante

---
+++ app/src/main/java/com/example/musicappjgis/nav/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.musicappjgis.nav
+
+object Routes {
+    const val Home = "home"
+    const val Detail = "detail"
+}
---
+++ app/src/main/java/com/example/musicappjgis/ui/components/MiniPlayer.kt
@@ -0,0 +1,7 @@
+package com.example.musicappjgis.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MiniPlayer() {
+}
